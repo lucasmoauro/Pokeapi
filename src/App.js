decb549b2d11fd43ee9860887a421b33c0846770
Lucas Moauro <lmoauro6@gmail.com>
2022-05-04 19:34:28 -0300

    Navbar + themeProvider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,46 @@
 import React from "react";
+import { Navbar } from "./components/Navbar";
+import { ThemeProvider, createTheme } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+
+const darkTheme = createTheme({
+	palette: {
+		mode: "dark",
+	},
+	components: {
+		MuiButton: {
+			styleOverrides: {
+				text: {
+					color: "#fff",
+				},
+			},
+		},
+	},
+});
+const lightTheme = createTheme({
+	palette: {
+		mode: "light",
+	},
+	components: {
+		MuiButton: {
+			styleOverrides: {
+				text: {
+					color: "#000",
+				},
+			},
+		},
+	},
+});
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<ThemeProvider theme={darkTheme}>
+				<CssBaseline />
+				<Navbar />
+			</ThemeProvider>
+		</>
+	);
 }
 
 export default App;
